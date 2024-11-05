27704b71e87a46bed452d1bbbc966d4243bf8971
Megan Liu <liumegan@microsoft.com>
2024-11-05 10:13:38 -0800

    remove unused use statement

--- rustls-symcrypt/src/verify.rs
+++ rustls-symcrypt/src/verify.rs
@@ -6,7 +6,7 @@ use symcrypt::ecc::{CurveType, EcKey, EcKeyUsage};
 use symcrypt::hash::{sha256, sha384, sha512, HashAlgorithm};
 use symcrypt::rsa::{RsaKey, RsaKeyUsage};
 use webpki::alg_id::{self};
-use pkcs1::RsaPrivateKey;
+
 
 /// Rsa signatures from the wire will come in the following ASN1 format:
 /// RSAPublicKey ::= SEQUENCE {
