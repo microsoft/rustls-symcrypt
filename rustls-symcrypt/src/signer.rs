14bc3b0bc4cad287371e85c0f50c861a70955a87
Megan Liu <liumegan@microsoft.com>
2024-11-07 15:54:53 -0800

    done with signer.rs and testing

--- rustls-symcrypt/src/signer.rs
+++ rustls-symcrypt/src/signer.rs
@@ -3,27 +3,31 @@
 /// https://github.com/microsoft/rust-symcrypt/blob/main/rust-symcrypt/src/rsa/mod.rs
 /// https://docs.rs/pkcs8/latest/pkcs8/trait.DecodePrivateKey.html#tymethod.from_pkcs8_der
 /// https://docs.rs/pkcs1/latest/pkcs1/trait.DecodeRsaPrivateKey.html#tymethod.from_pkcs1_der
-/// 
+/// https://docs.rs/der/latest/der/trait.Encode.html
+///
 use rustls::crypto::KeyProvider;
 use rustls::pki_types::PrivateKeyDer;
+use rustls::sign::Signer;
 use rustls::sign::SigningKey;
 use rustls::Error;
 use rustls::{SignatureAlgorithm, SignatureScheme};
-use rustls::sign::Signer;
 use symcrypt::{
+    ecc::{CurveType, EcKey, EcKeyUsage},
+    hash::{sha256, sha384, sha512, HashAlgorithm},
     rsa::{RsaKey, RsaKeyUsage},
-    ecc::{EcKey, CurveType, EcKeyUsage},
-    hash::{HashAlgorithm, sha256, sha384, sha512},
 };
 
-use std::sync::Arc;
-use std::fmt::Debug;
+use pkcs1::der::Decode;
 use pkcs1::RsaPrivateKey;
 use pkcs8::PrivateKeyInfo;
-use pkcs1::der::Decode;
 use sec1::der::Decodable;
 use sec1::EcPrivateKey;
+use std::fmt::Debug;
+use std::sync::Arc;
 
+use pkcs1::RsaPublicKey as ECSignatureData;
+use der::Encode;
+use pkcs1::UintRef;
 
 pub fn any_supported_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
     if let Ok(rsa) = RsaSigningKey::new(der) {
@@ -38,7 +42,6 @@ pub fn any_supported_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>
         "failed to parse private key as RSA or ECDSA ".into(),
     ))
 }
-
 
 pub fn any_ecdsa_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
     if let Ok(ecdsa_p256) = EcdsaSigningKey::new(
@@ -70,10 +73,9 @@ pub fn any_ecdsa_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Er
     ))
 }
 
-
 #[derive(Debug)]
-pub struct RsaSigningKey { 
-    key: Arc<RsaKey>
+pub struct RsaSigningKey {
+    key: Arc<RsaKey>,
 }
 
 static ALL_RSA_SCHEMES: &[SignatureScheme] = &[
@@ -84,7 +86,6 @@ static ALL_RSA_SCHEMES: &[SignatureScheme] = &[
     SignatureScheme::RSA_PKCS1_SHA384,
     SignatureScheme::RSA_PKCS1_SHA256,
 ];
-
 
 impl RsaSigningKey {
     pub fn new(der: &PrivateKeyDer<'_>) -> Result<Self, Error> {
@@ -104,7 +105,8 @@ impl RsaSigningKey {
                     private_key.prime1.as_bytes(),
                     private_key.prime2.as_bytes(),
                     RsaKeyUsage::Sign,
-                ).map_err(|_| Error::General("Failed to set RsaKey from PKCS#1".into()))?
+                )
+                .map_err(|_| Error::General("Failed to set RsaKey from PKCS#1".into()))?
             }
 
             PrivateKeyDer::Pkcs8(pkcs8) => {
@@ -123,16 +125,17 @@ impl RsaSigningKey {
                     private_key.prime1.as_bytes(),
                     private_key.prime2.as_bytes(),
                     RsaKeyUsage::Sign,
-                ).map_err(|_| Error::General("Failed to set RsaKey from PKCS#8".into()))?
+                )
+                .map_err(|_| Error::General("Failed to set RsaKey from PKCS#8".into()))?
             }
             _ => {
-                return Err(Error::General("Failed to parse RSA private key as either PKCS#1 or PKCS#8".into()));
+                return Err(Error::General(
+                    "Failed to parse RSA private key as either PKCS#1 or PKCS#8".into(),
+                ));
             }
         };
 
-        Ok(Self {
-            key: Arc::new(key),
-        })
+        Ok(Self { key: Arc::new(key) })
     }
 }
 
@@ -141,10 +144,12 @@ impl SigningKey for RsaSigningKey {
         ALL_RSA_SCHEMES
             .iter()
             .find(|scheme| offered.contains(scheme))
-            .map(|&scheme| Box::new(RsaSigner {
-                key: Arc::clone(&self.key),
-                scheme,
-            }) as Box<dyn Signer>)
+            .map(|&scheme| {
+                Box::new(RsaSigner {
+                    key: Arc::clone(&self.key),
+                    scheme,
+                }) as Box<dyn Signer>
+            })
     }
 
     fn algorithm(&self) -> SignatureAlgorithm {
@@ -160,11 +165,11 @@ struct RsaSigner {
 
 impl Signer for RsaSigner {
     fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
-        // from Ring, do we need to add padding??? 
+        // from Ring, do we need to add padding???
 
-        // Sign msg. msg is digested using the digest algorithm from padding_alg and the digest is then padded using the padding algorithm from padding_alg. 
+        // Sign msg. msg is digested using the digest algorithm from padding_alg and the digest is then padded using the padding algorithm from padding_alg.
         // The signature it written into signature; signature's length must be exactly the length returned by public_modulus_len().
-        // Many other crypto libraries have signing functions that takes a precomputed digest as input, 
+        // Many other crypto libraries have signing functions that takes a precomputed digest as input,
         // instead of the message to digest. This function does not take a precomputed digest; instead, sign calculates the digest itself.
 
         match self.scheme {
@@ -191,27 +196,38 @@ impl Signer for RsaSigner {
             }
             SignatureScheme::RSA_PSS_SHA256 => {
                 let hashed_message = sha256(message);
-                match self.key.pss_sign(&hashed_message, HashAlgorithm::Sha256, 32) {
+                match self
+                    .key
+                    .pss_sign(&hashed_message, HashAlgorithm::Sha256, 32)
+                {
                     Ok(signature) => Ok(signature),
                     Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
                 }
             }
             SignatureScheme::RSA_PSS_SHA384 => {
                 let hashed_message = sha384(message);
-                match self.key.pss_sign(&hashed_message, HashAlgorithm::Sha384, 48) {
+                match self
+                    .key
+                    .pss_sign(&hashed_message, HashAlgorithm::Sha384, 48)
+                {
                     Ok(signature) => Ok(signature),
                     Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
                 }
             }
             SignatureScheme::RSA_PSS_SHA512 => {
                 let hashed_message = sha512(message);
-                match self.key.pss_sign(&hashed_message, HashAlgorithm::Sha512, 64) {
+                match self
+                    .key
+                    .pss_sign(&hashed_message, HashAlgorithm::Sha512, 64)
+                {
                     Ok(signature) => Ok(signature),
                     Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
                 }
             }
             _ => {
-                return Err(Error::General("unsupported RSA-PKCS1 signature scheme".into()));
+                return Err(Error::General(
+                    "unsupported RSA-PKCS1 signature scheme".into(),
+                ));
             }
         }
     }
@@ -221,14 +237,12 @@ impl Signer for RsaSigner {
     }
 }
 
-
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct EcdsaSigningKey {
-    key: Arc<EcKey>, 
+    key: Arc<EcKey>,
     scheme: SignatureScheme,
 }
-
 
 impl EcdsaSigningKey {
     /// Creates a new `ECDSASigningKey` from DER encoding in either PKCS#8 or SEC1
@@ -257,11 +271,15 @@ impl EcdsaSigningKey {
                     .map_err(|_| Error::General("Failed to parse SEC1 DER".into()))?;
 
                 // Use EcPrivateKey's private_key to set up the ECDSA key
-                EcKey::set_key_pair(curve_type, &private_key.private_key, None, EcKeyUsage::EcDsa)
-                    .map_err(|_| Error::General("Failed to set ECDSA key from SEC1".into()))?
+                EcKey::set_key_pair(
+                    curve_type,
+                    &private_key.private_key,
+                    None,
+                    EcKeyUsage::EcDsa,
+                )
+                .map_err(|_| Error::General("Failed to set ECDSA key from SEC1".into()))?
             }
             PrivateKeyDer::Pkcs8(pkcs8) => {
-
                 // Extract DER-encoded private key blob for PKCS#8
                 let private_key_blob = pkcs8.secret_pkcs8_der();
                 // Parse the DER-encoded private key
@@ -271,10 +289,19 @@ impl EcdsaSigningKey {
                     .map_err(|_| Error::General("Failed to parse PKCS#8 DER".into()))?;
 
                 // Use EcPrivateKey's private_key to set up the ECDSA key
-                EcKey::set_key_pair(curve_type, &private_key.private_key, None, EcKeyUsage::EcDsa)
-                    .map_err(|_| Error::General("Failed to set ECDSA key from PKCS#8".into()))?
+                EcKey::set_key_pair(
+                    curve_type,
+                    &private_key.private_key,
+                    None,
+                    EcKeyUsage::EcDsa,
+                )
+                .map_err(|_| Error::General("Failed to set ECDSA key from PKCS#8".into()))?
             }
-            _ => return Err(Error::General("Invalid key format: must be PKCS#1 or PKCS#8".into())),
+            _ => {
+                return Err(Error::General(
+                    "Invalid key format: must be PKCS#1 or PKCS#8".into(),
+                ))
+            }
         };
 
         // Return the ECDSASigningKey with Arc-wrapped key_pair and scheme
@@ -311,32 +338,36 @@ struct EcdsaSigner {
 
 impl Signer for EcdsaSigner {
     fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
-        match self.scheme { 
-            SignatureScheme::ECDSA_NISTP256_SHA256 => {
-                let hash_value = sha256(message);
-                match self.key.ecdsa_sign(&hash_value) {
-                    Ok(signature) => Ok(signature),
-                    Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
-                }
-            }
-            SignatureScheme::ECDSA_NISTP384_SHA384 => {
-                let hashed_message = sha384(message);
-                match self.key.ecdsa_sign(&hashed_message) {
-                    Ok(signature) => Ok(signature),
-                    Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
-                }
-            }
-            SignatureScheme::ECDSA_NISTP521_SHA512 => {
-                let hashed_message = sha512(message);
-                match self.key.ecdsa_sign(&hashed_message) {
-                    Ok(signature) => Ok(signature),
-                    Err(e) => Err(Error::General(format!("failed to sign message: {}", e))),
-                }
-            }
-            _ => {
-                return Err(Error::General("unsupported ECDSA signature scheme".into()));
-            }
-        }
+        // Step 1: Hash the message based on the scheme
+        let hash_value = match self.scheme {
+            SignatureScheme::ECDSA_NISTP256_SHA256 => sha256(message).to_vec(),
+            SignatureScheme::ECDSA_NISTP384_SHA384 => sha384(message).to_vec(),
+            SignatureScheme::ECDSA_NISTP521_SHA512 => sha512(message).to_vec(),
+            _ => return Err(Error::General("unsupported ECDSA signature scheme".into())),
+        };
+
+        // Step 2: Sign the hashed message
+        let signature = self
+            .key
+            .ecdsa_sign(&hash_value)
+            .map_err(|e| Error::General(format!("failed to sign message: {}", e)))?;
+
+        // Step 3: Split the signature into r and s components
+        let (r, s) = signature.split_at(signature.len() / 2);
+
+        // Step 4: Create an RsaPublicKey structure with r and s
+        let ec_sig_data = ECSignatureData {
+            modulus: UintRef::new(r).unwrap(),
+            public_exponent: UintRef::new(s).unwrap(),
+        };
+
+        // Step 5: Encode the RsaPublicKey using the Encode trait
+        let mut encoded_signature = Vec::new();
+        ec_sig_data
+            .encode_to_vec(&mut encoded_signature)
+            .map_err(|e| Error::General(format!("failed to encode signature: {}", e)))?;
+
+        Ok(encoded_signature)
     }
 
     fn scheme(&self) -> SignatureScheme {
@@ -356,5 +387,3 @@ impl KeyProvider for SymCryptProvider {
         any_supported_type(&key_der)
     }
 }
-
-
